e97590f125dd53f0bdb7b8671232841c16a2d992
omolaja <dipo142008@gmail.com>
2025-03-20 15:16:19 +0000

    Update AppDbContext.cs

--- CardScheme.Infrastructure/Context/AppDbContext.cs
+++ CardScheme.Infrastructure/Context/AppDbContext.cs
@@ -4,9 +4,19 @@ using Microsoft.EntityFrameworkCore;
 
 namespace CardScheme.Infrastructure.AppContext
 {
-	public class AppDbContext : DbContext
-	{
-		DbSet<Customers> customers { get; set; }
-	}
+	       public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+		{
+
+		}
+
+		public DbSet<Customers> Customers { get; set; }
+
+		public DbSet<Gender> Gender { get; set; }
+
+		public DbSet<CardTypes> CardTypes { get; set; }
+
+		public DbSet<CustomerAddress> CustomerAddresses { get; set; }
+
+		public DbSet<CustomerCards> CustomerCards { get; set; }
 }
 
