709df6d5497ecc1d17319b0697b015897702912f
dipo142008@gmail.com <dipo142008@gmail.com>
2025-03-20 15:54:34 +0000

    Update On DbContext

--- CardScheme.Infrastructure/Context/AppDbContext.cs
+++ CardScheme.Infrastructure/Context/AppDbContext.cs
@@ -4,30 +4,21 @@ using Microsoft.EntityFrameworkCore;
 
 namespace CardScheme.Infrastructure.AppContext
 {
-<<<<<<< Updated upstream
-	       public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
-=======
-	public class AppDbContext : DbContext
-	{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
 
-		public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
->>>>>>> Stashed changes
-		{
+        }
+        public DbSet<Customers> Customers { get; set; }
 
-		}
+        public DbSet<Gender> Gender { get; set; }
 
-		public DbSet<Customers> Customers { get; set; }
+        public DbSet<CardTypes> CardTypes { get; set; }
 
-		public DbSet<Gender> Gender { get; set; }
+        public DbSet<CustomerAddress> CustomerAddresses { get; set; }
 
-		public DbSet<CardTypes> CardTypes { get; set; }
-
-		public DbSet<CustomerAddress> CustomerAddresses { get; set; }
-
-		public DbSet<CustomerCards> CustomerCards { get; set; }
-<<<<<<< Updated upstream
-=======
-	}
->>>>>>> Stashed changes
+        public DbSet<CustomerCards> CustomerCards { get; set; }
+    }
 }
 
